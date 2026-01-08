569e12c6233d978d2a21ee97e9dd7d9fa6ae5f4a
devfancy <fancy.junyongmoon@gmail.com>
2026-01-08 23:06:06 +0900

    feat: 애플리케이션 클래스 생성
    Co-Authored-By: gaonnuri <chy.song89@gmail.com>

---
+++ src/main/java/wordle/Application.java
@@ -0,0 +1,10 @@
+package wordle;
+
+public class Application {
+
+    public static void main(String[] args) {
+        // 게임 준비 -> 객체 생성
+        // 입력, 출력 -> 객체 생성
+        // 게임 시작 wordle.start()
+    }
+}
