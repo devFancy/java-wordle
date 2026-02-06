29a3c50bc3d9e2607247344d3e5bad6af1737d72
devfancy <fancy.junyongmoon@gmail.com>
2026-02-06 10:32:13 +0900

    refactor: 파일 읽기 중 발생하는 에러에 대해 메시지 및 에러 남기도록 수정

--- src/main/java/wordle/exception/FileReadFailException.java
+++ src/main/java/wordle/exception/FileReadFailException.java
@@ -1,4 +1,12 @@
 package wordle.exception;
 
 public class FileReadFailException extends RuntimeException{
+
+    public FileReadFailException(final String message) {
+        super(message);
+    }
+
+    public FileReadFailException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
 }
--- src/main/java/wordle/util/FileReader.java
+++ src/main/java/wordle/util/FileReader.java
@@ -16,12 +16,12 @@ public class FileReader {
     // NOTE: 파일을 하나씩 읽고 리스트로 반환
     public List<String> readByLine(final String filePath) {
         if (Objects.isNull(filePath) || filePath.isBlank()) {
-            throw new FileReadFailException();
+            throw new FileReadFailException("파일 경로가 비어있습니다.");
         }
         try (final Stream<String> lines = Files.lines(getFilePath(filePath))) {
             return lines.toList();
         } catch (IOException | URISyntaxException exception) {
-            throw new FileReadFailException();
+            throw new FileReadFailException("파일 읽기 중 에러가 발생했습니다. path: " + filePath, exception);
         }
     }
 
@@ -32,7 +32,7 @@ public class FileReader {
     private URL getUrl(final String filePath) {
         final URL systemResource = ClassLoader.getSystemResource(filePath);
         if (Objects.isNull(systemResource)) {
-            throw new FileReadFailException();
+            throw new FileReadFailException("해당 경로의 파일을 찾을 수 없습니다. path: " + filePath);
         }
         return systemResource;
     }
